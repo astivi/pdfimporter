4ed91cba64379b4296eda27ca8900ecae9218a15
Álvaro Almeida Freire Stivi <alvaro.stivi@gmail.com>
2016-10-11 00:21:24 -0300

    Adds project files

---
+++ lib/importers/pdf_ecad.rb
@@ -0,0 +1,14 @@
+module Importers
+  class PdfEcad
+    CATEGORIES = {"CA" => "Author", "E" => "Publisher", "V" => "Versionist", "SE" => "SubPublisher"}
+
+    def works
+    end
+
+    def right_holder(line)
+    end
+
+    def work(line)
+    end
+  end
+end
